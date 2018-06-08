ee44824291ee21cea0a1c7f5f3e961e3496ea52a
jpike <jacob@jacobpike.com>
2018-06-08 19:51:58 -0400

    Adding basic static analysis compiler options (commented out).

--- build.bat
+++ build.bat
@@ -10,6 +10,8 @@ REM If not specified, will default to debug.
 SET build_mode=%1
 
 REM DEFINE COMPILER OPTIONS.
+REM Commented out options below are for static analysis.
+REM SET COMMON_COMPILER_OPTIONS=/EHsc /WX /W4 /TP /analyze:WX- /analyze:plugin EspXEngine.dll
 SET COMMON_COMPILER_OPTIONS=/EHsc /WX /W4 /TP
 SET DEBUG_COMPILER_OPTIONS=%COMMON_COMPILER_OPTIONS% /Z7 /Od /MTd
 SET RELEASE_COMPILER_OPTIONS=%COMMON_COMPILER_OPTIONS% /O2 /MT
@@ -22,6 +24,13 @@ SET LIBRARIES=user32.lib gdi32.lib opengl32.lib
 REM CREATE THE COMMAND LINE OPTIONS FOR THE FILES TO COMPILE/LINK.
 SET INCLUDE_DIRS=/I %MAIN_CODE_DIR%
 SET PROJECT_FILES_DIRS_AND_LIBS=%COMPILATION_FILE% %INCLUDE_DIRS% /link %LIBRARIES%
+
+REM SET VARIABLES FOR CODE ANALYSIS.
+REM Commented out by default.
+REM SET esp.extensions=cppcorecheck.dll
+REM SET esp.annotationbuildlevel=ignore
+REM This seems to allow ignoring of standard lib includes.
+REM SET caexcludepath=%include%
 
 REM MOVE INTO THE BUILD DIRECTORY.
 IF NOT EXIST "build" MKDIR "build"
